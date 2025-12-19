a9517ad5961f4613765503e55122538d7d4b70e7
bilalkhalid20045 <bilalkhalid20045@gmail.com>
2025-12-19 10:41:10 +0500

    Add files via upload

---
+++ CountEvenOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class CountEvenOdd
+{
+   static void Main()
+   {
+       Console.WriteLine("Enter 10 numbers separated by spaces:");
+       string input = Console.ReadLine();           // Read the full line
+       string[] parts = input.Split(' ');           // Split by spaces
+
+       int evenCount = 0, oddCount = 0;
+
+       for (int i = 0; i < parts.Length; i++)       // Loop through all numbers
+       {
+           int num = Convert.ToInt32(parts[i]);     // Convert each part to an integer
+
+           if (num % 2 == 0)
+               evenCount++;
+           else
+               oddCount++;
+       }
+
+       Console.WriteLine("\nTotal Even numbers: " + evenCount);
+       Console.WriteLine("Total Odd numbers: " + oddCount);
+   }
+}
+
